087fe2828d3da614dcaf66d54c5ef415c9fb1eda
Paulinho <paulocardosoassessor@gmail.com>
2023-10-27 21:36:28 -0300

    first commit

---
+++ Projetos/Aula_01_11/src/aula_01/Aula_01.java
@@ -0,0 +1,20 @@
+package aula_01;
+
+public class Aula_01 {
+
+	public static void main(String[] args) {
+		
+		// Declaração de variável
+		int valor;
+		
+		// Declaração de variáveis
+		int num1, num2, num3;
+		char caractere;
+		float preco;
+		
+		// Declarando e inicializando variável
+		int num4 = 100;
+		
+
+	}
+}
