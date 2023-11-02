71379186484297f250cbce113886e932d50776be
Paulinho <paulocardosoassessor@gmail.com>
2023-11-02 10:09:19 -0300

    Exercicio 8 - Transformar Kelvin e Celsius e mostrar na tela

--- Projetos/Exercicio_8_aula_14_Kelvin_em_Celsius/src/aula_14/Kelvin_Celsius.java
+++ Projetos/Exercicio_8_aula_14_Kelvin_em_Celsius/src/aula_14/Kelvin_Celsius.java
@@ -3,7 +3,7 @@ package aula_14;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Fahrenheit_Celsius {
+public class Kelvin_Celsius {
 
 	public static void main(String[] args) {
 		
@@ -12,15 +12,15 @@ public class Fahrenheit_Celsius {
 		// TROCARÁ A VÍRGULA (,) POR PONTO (.)
 		Locale.setDefault(Locale.US);
 		
-		System.out.println("Faça um programa que leia a temperatura em Fahrenheit e transforme em Celsius.");
+		System.out.println("Faça um programa que leia a temperatura em Kelvin e transforme em Celsius.");
 		System.out.println();
 		
 		System.out.print("Digite o número: ");
-		float fahrenehit = sc.nextFloat();
+		float kelvin = sc.nextFloat();
 		
-		float celsius = 5 * (fahrenehit - 32) / 9;
+		double celsius = kelvin - 273.15;
 
-		System.out.println("A temperatura em Fahrenheit é " + celsius);
+		System.out.println("A temperatura em Kelvin é " + kelvin);
 		System.out.printf("A temperatura transformada em Celsius é, " + celsius);
 		
 		sc.close();
