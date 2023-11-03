7110cd2041e02839ca0735e9003301baa4fef883
Paulinho <paulocardosoassessor@gmail.com>
2023-11-03 12:13:51 -0300

    Exercicio 23 - Transformar Metros em Jardas e mostrar na tela

---
+++ Projetos/Exercicio_23_aula_14_Metros_Jardas/src/aula_14/Metros_Jarbas.java
@@ -0,0 +1,28 @@
+package aula_14;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Metros_Jarbas {
+
+	public static void main(String[] args) {
+	
+		// Locale SUBSTITUIRÁ A VÍRGULA (,) POR PONTO (.)
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Fazer um programa para ler um valor em Metros e transformar em Jardas");
+		System.out.println();
+		
+		System.out.print("Digite o valor em Jardas: ");
+		double metros = sc.nextDouble();
+		
+		double jardas = metros / 0.91;
+		
+		System.out.println("O valor digitado em Metros é de: " + metros);
+		// AO COLOCAR %.4f%n SERÁ IMPRESSO APENAS 4 CASAS APÓS A VÍRGULA
+		System.out.printf("O valor trasnformado em Jardas será de: %.4f%n", jardas);
+		
+		sc.close();
+	}
+}
