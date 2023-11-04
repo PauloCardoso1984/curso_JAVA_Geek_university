b0f77026b1a1a5ba7941b1e214674c7d8dd195cf
Paulinho <paulocardosoassessor@gmail.com>
2023-11-04 13:23:41 -0300

    Exercicio 36 - Ler um valor de altura e raio de um cilindro e mostra a capacidade em m3

---
+++ Projetos/Exercicio_36_aula_14_Area_do_Cilindro/src/aula_14/Area_Cilindro.java
@@ -0,0 +1,33 @@
+package aula_14;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Area_Cilindro {
+
+	public static void main(String[] args) {
+	
+		// Locale SUBSTITUIRÁ A VÍRGULA (,) POR PONTO (.)
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double PI = 3.141592;
+		
+		System.out.println("Fazer um programa que receba o valor da altura e raio de um cilindro e informe o seu volume.");
+		System.out.println();
+		
+		System.out.print("Digite o valor da altura: ");
+		double altura = sc.nextDouble();
+		
+		System.out.print("Digite o valor do raio: ");
+		double raio = sc.nextDouble();
+
+		// MÉTODO OBTER O RESULTADO
+		double volumecilindro =  PI * Math.pow(raio, 2) * altura;
+		
+		System.out.println("Valor digitado da altura: " + altura + ", raio: " + raio);
+		// DESTA FORMA IMPRIMIRÁ 2 CASAS APÓS A VÍRGULA
+		System.out.printf("Capacidade do cilindro é de: %.2f%n ", volumecilindro, "m3");
+		sc.close();
+	}
+}
