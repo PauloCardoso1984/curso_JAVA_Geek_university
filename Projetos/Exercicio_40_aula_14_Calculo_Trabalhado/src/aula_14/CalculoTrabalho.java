6ce072e214c287de092b5a3e472fc21f933e428b
Paulinho <paulocardosoassessor@gmail.com>
2023-11-08 20:54:39 -0300

    Exercicio 40 - Fazer um programa que lê uma quantidade de horas trabalhadas e calcula o salário descontando 8% do IR

---
+++ Projetos/Exercicio_40_aula_14_Calculo_Trabalhado/src/aula_14/CalculoTrabalho.java
@@ -0,0 +1,38 @@
+package aula_14;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculoTrabalho {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Faça um programa para empresa que contrata um encanador a R$ 30,00 por dia. \n"
+				+ "Faça um programa que solicite o número de dias trabalhados pelo encanador e imprima a \n"
+				+ "quantia que deverá ser paga, sabendo-se que são descontados 8% para imposto de renda.");
+		
+		System.out.println();
+		
+		// SUBSTITUIRÁ A VIRGULA POR PONTO
+		Locale.setDefault(Locale.US);
+		// INSTANCIANDO A COMUNICAÇÃO EXTERNA POR MEIO DO TECLADO
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Valor do contrato é do R$ 30,00 por dia trabalhado.");
+		System.out.print("Quantos dias o encanador trabalhou? ");
+		int diastrabalhados = sc.nextInt();
+		System.out.println();
+		
+		// CALCULANDO A PRÊMIAÇÃO
+		double totalpagar = diastrabalhados * 30.0;
+		double impostoderenda = totalpagar - (totalpagar * 0.08);
+	
+		// A COLOCAÇÃO DO %.2f%n SERVIRÁ PARA COLOCAR 02 CASAS APÓS A VIRGULA
+		System.out.println(" O funcionário trabalhou por " + diastrabalhados + " dias.");
+		System.out.printf(" O valor a receber (SEM DESCONTO) é R$ %.2f%n ", totalpagar);
+		System.out.printf("O valor a receber (COM DESCONTO) é R$ %.2f%n ", impostoderenda);
+	
+		sc.close();
+	}
+}
