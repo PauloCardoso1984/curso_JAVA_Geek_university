6c9c57e6c8c54b8551bf8005efb337763055add6
Paulinho <paulocardosoassessor@gmail.com>
2023-11-11 21:11:53 -0300

    Exercicio 47.3 - Faça um programa que leia uma senha numérica e que seja interrompido a execução com o acerto da senha

--- Projetos/Exercicio_47.3_aula_14_ValidarSenha/src/aula_14/ValidaSenha.java
+++ Projetos/Exercicio_47.3_aula_14_ValidarSenha/src/aula_14/ValidaSenha.java
@@ -2,24 +2,27 @@ package aula_14;
 
 import java.util.Scanner;
 
-public class UmNumeroPorLinha {
+public class ValidaSenha {
 
+	@SuppressWarnings("unlikely-arg-type")
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Faça um programa que leia uma senha para validar um acesso. Senha VALIDA é 1234.");
+		System.out.println("Faça um programa que leia uma senha para validar um acesso. A senha será fornecida pelo usuário (APENAS NÚMERO).");
 		System.out.println();
 
+		System.out.print("Cadastre a senha: ");
         // Defina a senha correta
-        String senhaCorreta = "1234";
-
+        int senha = sc.nextInt();
+        
+       
         // Repetição até que a senha seja fornecida corretamente
         while (true) {
-            System.out.print("Digite a senha: ");
-            String senhaDigitada = sc.nextLine();
+            System.out.print("Confirme a senha: ");
+            int senhaDigitada = sc.nextInt();
 
-            if (senhaDigitada.equals(senhaCorreta)) {
+            if (senhaDigitada == senha) {
                 System.out.println("Senha correta. Acesso liberado!");
                 break; // Encerra o loop quando a senha estiver correta
             } else {
