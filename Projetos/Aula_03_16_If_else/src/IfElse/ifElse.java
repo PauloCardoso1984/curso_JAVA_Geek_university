eb61735d48f3b221defead5880f16d97b5abfddc
Paulinho <paulocardosoassessor@gmail.com>
2023-11-16 19:56:15 -0300

    Aula 02 - If e Else

---
+++ Projetos/Aula_03_16_If_else/src/IfElse/ifElse.java
@@ -0,0 +1,26 @@
+package IfElse;
+
+import java.util.Scanner;
+
+public class ifElse {
+	public static void main(String[] args) {
+		
+	// DECLARANDO E INICIALIZANDO A VARIÁVEL
+	int numero = 29;
+	
+	// Se = if  - Senão = else
+	if (numero > 5 ) {
+		System.out.println("Sim, o numero " + numero + " é maior que 5.");	
+		}
+	if (numero > 9) {
+		System.out.println("O numero " + numero + " é maior ou igual a 9." );
+		}
+	else if  (numero > 11) {
+		System.out.println("Esse numero está fora do determinado.");
+		}
+	}
+}
+	
+	
+
+
