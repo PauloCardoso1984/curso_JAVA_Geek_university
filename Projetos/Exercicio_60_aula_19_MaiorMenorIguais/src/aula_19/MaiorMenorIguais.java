13126f4005cba66d912de5eda0cbb02daa5090e5
Paulinho <paulocardosoassessor@gmail.com>
2023-11-19 09:28:54 -0300

    Exercicio 60 - Ler 02 números e mostrar o maior e se for iguais, imprimir que são iguais

---
+++ Projetos/Exercicio_60_aula_19_MaiorMenorIguais/src/aula_19/MaiorMenorIguais.java
@@ -0,0 +1,34 @@
+package aula_19;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MaiorMenorIguais {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Leia um número fornecido pelo usuário. Verificar qual é o maior e se for \n"
+				+ "iguais imprimir que são iguais.");
+		System.out.println();
+
+		System.out.print("Digite o 1º número: ");
+		int numero1 = sc.nextInt();
+		
+		System.out.print("Digite o 2º número: ");
+		int numero2 = sc.nextInt();
+		
+		// VERIFICAÇÃO
+		if (numero1 < numero2) {
+			System.out.println("Maior número é " + numero2);
+		} 
+		if (numero1 > numero2) {
+			System.out.println("Maior número é " + numero1);
+		}
+		if (numero1 == numero2) {
+			System.out.println("Os números " + numero1 + " e " + numero2 + " são iguais.");	
+		}
+		sc.close();
+	}
+}
