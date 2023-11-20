bceb19d0c2446affb631034552ecd61c9993db49
Paulinho <paulocardosoassessor@gmail.com>
2023-11-20 16:47:40 -0300

    Exercicios 73 - Verificar qual é o triângulo

---
+++ Projetos/Exercicio_73_aula_19_Verificar_Triangulo/src/aula_19/Program.java
@@ -0,0 +1,38 @@
+package aula_19;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dados três valores A,B,C verificar se eles podem ser valores dos lados \n"
+        		+ "de um triangulo e, se forem, se é um triangulo escaleno, equilátero ou isosceles.");
+        System.out.println();
+        
+        System.out.println("Digite o valor dos lados do triângulo.");
+        
+        double ladoA;
+        double ladoB;
+        double ladoC;
+        
+        System.out.println("Lado A: ");
+        ladoA = sc.nextDouble();
+        System.out.println("Lado B: ");
+        ladoB = sc.nextDouble();
+        System.out.println("Lado C: ");
+        ladoC = sc.nextDouble();
+        
+        if (ladoA == ladoB && ladoA == ladoC && ladoB == ladoC && ladoC == ladoA) {        	
+        	System.out.println("Esse triangulo é o EQUILÁTERO");
+        }
+        if (ladoA == ladoB && ladoB != ladoC && ladoC == ladoA || ladoA != ladoB && ladoA == ladoC || ladoA != ladoB && ladoB == ladoC || ladoA == ladoB && ladoB != ladoC) {
+        	System.out.println("Esse triangulo é o ISOSCELES");
+        }
+        if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC && ladoC != ladoA) {
+        	System.out.println("Esse triangulo é o ESCALENO");
+        }
+    }
+}
