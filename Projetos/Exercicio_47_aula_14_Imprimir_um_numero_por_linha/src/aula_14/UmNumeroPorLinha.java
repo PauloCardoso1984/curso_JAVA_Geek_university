462b1b52d90e491c5be13cdad42b92dc6b0c3243
Paulinho <paulocardosoassessor@gmail.com>
2023-11-20 21:37:10 -0300

    Exercicios 82.1, 82.2, 82.3 e 82.4 - Prova infantil de matemática

--- Projetos/Exercicio_47_aula_14_Imprimir_um_numero_por_linha/src/aula_14/UmNumeroPorLinha.java
+++ Projetos/Exercicio_47_aula_14_Imprimir_um_numero_por_linha/src/aula_14/UmNumeroPorLinha.java
@@ -8,7 +8,7 @@ public class UmNumeroPorLinha {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Faça um programa que leia um número inteiro positico em imprima de \n"
+		System.out.println("Faça um programa que leia um número inteiro positivo em imprima de \n"
 				+ "1 até o número e do número até 0.");
 		System.out.println();
 
@@ -17,8 +17,9 @@ public class UmNumeroPorLinha {
 		int numero = n;
 		System.out.println("Numeros de 1 a " + n + ":"); // + é o operador de concatenação de strings
 		
-		for (int i = 1; i <= n; i++)
+		for (int i = 0; i <= n; i++)
 			System.out.println(i);
+		System.err.println();
 		System.out.println("Numeros inversos de valor digitado até 0:");
 		for (int i = n; i >= 0; i--)
 			System.out.println(i);
---
+++ Projetos/Exercicio_82.3_aula_19_Calculos_Multiplicação_Crianças/src/aula_19/Programa.java
@@ -0,0 +1,44 @@
+package aula_19;
+
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Random rand = new Random();
+
+		System.out.println(
+				"Faça uma prova de matemática para crianças que estão aprendendo a multiplicar números inteiros \n"
+				+ "menores do que 100. Escolha números aleatórios entre 1 e 100, e mostre na tela a pergunta: \n"
+				+ "qual é a soma de a * b, onde a e b são os números aleatórios. Peça a resposta. Faça cinco \n"
+				+ "perguntas ao aluno, e mostre para ele as perguntas e as respostas corretas, além de quantas \n"
+				+ "vezes o aluno acertou.");
+		System.out.println();
+
+		int acertos = 0;
+
+		for (int i = 0; i < 5; i++) {
+			int a = rand.nextInt(100) + 1;
+			int b = rand.nextInt(100) + 1;
+			int resposta;
+
+			System.out.print("Qual é a subtração de " + a + " - " + b + "? ");
+			resposta = sc.nextInt();
+
+			if (resposta == a - b) {
+				System.out.println("Resposta correta!");
+				acertos++;
+			} else {
+				System.out.println("Resposta incorreta! A resposta correta é " + (a - b));
+			}
+			System.out.println();
+		}
+		System.out.println("Você acertou " + acertos + " perguntas.");
+	}
+}
