3cfeae7be44c5ef5ce4211fd2fcde6c6e0640e4c
Paulinho <paulocardosoassessor@gmail.com>
2023-11-25 10:47:12 -0300

    Exercicios 84 - Faça um programa que receba altura e peso, depois classifique

--- Projetos/Exercicio_88_aula_19_Data/src/aula_19/Programa.java
+++ Projetos/Exercicio_88_aula_19_Data/src/aula_19/Programa.java
@@ -12,57 +12,48 @@ public class Programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Leia uma data e determine se ela é válida, ou seja, verifique se o mês está entre 1 e 12, e se o dia existe naquele mês. Note que Fevereiro tem 29 dias em anos bissextos, e 28 dias nos demais.");
+		System.out.println("Escreva um programa que, dado o valor da venda immprima a comissão. \n"
+				+ "VENDA MENSAL				*COMISSÃO\n"
+				+ "+= R$ 100.000,00			R$ 700,00 + 16% das vendas \n"
+				+ "- R$ 100.000,00	+ 80.000,00		R$ 650,00 + 14% das vendas \n"
+				+ "- R$ 80.000,00 + 60.000,00		R$ 600,00 + 14% das vendas \n"
+				+ "- R$ 60.000,00 + 40.000,00		R$ 550,00 + 14% das vendas \n"
+				+ "- R$ 40.000,00 + 20.000,00		R$ 500,00 + 14% das vendas \n"
+				+ "- R$ 20.000,000				R$ 400,00 + 14% das vendas");
 		System.out.println();
-
-        System.out.println("Digite uma data no formato dd/mm/aaaa: ");
-        String dataString = sc.nextLine();
-        sc.close();
-        
-        // Verificar se a data possui 10 caracteres
-        if (dataString.length() != 10) {
-            System.out.println("Data inválida!");
-            return;
-        }
-        
-        // Extrair dia, mês e ano da string
-        int dia = Integer.parseInt(dataString.substring(0, 2));
-        int mes = Integer.parseInt(dataString.substring(3, 5));
-        int ano = Integer.parseInt(dataString.substring(6, 10));
-        
-        // Verificar se o ano é bissexto
-        boolean bissexto = (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
-        
-        // Verificar se o mês está entre 1 e 12
-        if (mes < 1 || mes > 12) {
-            System.out.println("Data inválida!");
-            return;
-        }
-        
-        // Verificar se o dia está dentro do intervalo correto para cada mês
-        int maxDia;
-        switch (mes) {
-            case 2:
-                maxDia = bissexto ? 29 : 28;
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                maxDia = 30;
-                break;
-            default:
-                maxDia = 31;
-                break;
-        }
-        
-        if (dia < 1 || dia > maxDia) {
-            System.out.println("Data inválida!");
-            return;
-        }
-        
-        System.out.println("Data válida!");
-        sc.close();
+		
+		System.out.println("Informe seu nome: ");
+		String nome = sc.nextLine();
+		
+		System.out.println("Qual o valor total de venda R$ ");
+		double valor = sc.nextDouble();
+		
+		// VERIFICAÇÃO
+		if (valor > 100.000) {
+			double pagar = 700.0 + (valor / 100 * 16);
+			System.out.println(nome + " você receberá R$ " + pagar + " PARABÉNS");
+		}
+		else if (valor >= 100.000 && valor <= 80.000) {
+			double pagar = 650.0 + (valor * 0.14);
+			System.out.println(nome + " você receberá R$ " + pagar + " MUITO BOM");
+		}
+		else if (valor > 80.000 && valor <= 60.000) {
+			double pagar = 600.0 + (valor * 0.14);
+			System.out.println(nome + " você receberá R$ " + pagar + " BOM");
+		}
+		else if (valor > 60.000 && valor <= 40.000) {
+			double pagar = 550.0 + (valor * 0.14);
+			System.out.println(nome + " você receberá R$ " + pagar + " PRECISA MELHORAR");
+		}
+		else if (valor > 40.000 && valor <= 20.000) {
+			double pagar = 500.0 + (valor * 0.14);
+			System.out.println(nome + " você receberá R$ " + pagar + " MUITO RUIM");
+		}
+		else if (valor <= 20.000) {
+			double pagar = 400.0 + (valor * 0.14);
+			System.out.println(nome + " você receberá R$ " + pagar + " PÉSSIMO");
+		}
+		sc.close();
     }
 }
 
---
+++ Projetos/Exercicio_89_aula_19_Tarifa_Estacionamento/src/aula_19/Programa.java
@@ -0,0 +1,62 @@
+package aula_19;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("As tarifas de certo estacionamento são as seguintes. \n" + " 1 e 2 horas		R$ 1.00 \n"
+				+ " 3 e 4 horas 		R$ 1.4 \n" + " 5 horas a seguir	R$ 2.0");
+		System.out.println();
+
+		// Definir o formato desejado para a data e hora
+		SimpleDateFormat formatoDataHora = new SimpleDateFormat("HH:mm");
+
+		// Ler a data e hora de entrada e saída do estacionamento do usuário
+		System.out.print("Informe a hora de entrada (HH:mm): ");
+		String entradaStr = sc.nextLine();
+
+		System.out.print("Informe a data e hora de saída (dd/MM/yyyy HH:mm): ");
+		String saidaStr = sc.nextLine();
+
+		double tarifa = 0;
+
+		try {
+			// Converter as strings de entrada e saída para objetos Date
+			Date entrada = formatoDataHora.parse(entradaStr);
+			Date saida = formatoDataHora.parse(saidaStr);
+
+			// Calcular a diferença de horas entre a entrada e saída
+			long duracaoSegundos = saida.getTime() - entrada.getTime();
+			int duracaoHoras = (int) (duracaoSegundos / 1000 / 60 / 60);
+
+			// Calcular a tarifa com base na quantidade de horas
+
+			if (duracaoHoras <= 2) {
+				tarifa = duracaoHoras * 1.00;
+			} else if (duracaoHoras <= 4) {
+				tarifa = duracaoHoras * 1.40;
+			} else if (duracaoHoras >= 5) {
+				tarifa = duracaoHoras * 2.00;
+			}
+			if (duracaoHoras < 0) {
+				System.out.println("Valor inválido!! ERRO");
+				sc.close();
+			}
+				// Imprimir a tarifa calculada
+				System.out.println("A tarifa total é R$ " + tarifa);
+			
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro ao efetuar o cálculo das tarifas.");
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+}
