6c0ee8a4c25c2f5303e94084cbea922fff38ede6
Paulinho <paulocardosoassessor@gmail.com>
2023-11-30 21:49:26 -0300

    Exercicios 109 - Faça um programa que leia um número inteiro positivo impar N e imprima todos os números naturais impares de 1 até N em ordem decrescente.

---
+++ Projetos/Exercicio_109_aula_19_Ordem_Decescente_Impar/src/aula_19/Programa.java
@@ -0,0 +1,25 @@
+package aula_19;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Faça um programa que leia um número inteiro positivo impar N e imprima todos os números \n"
+				+ "naturais impares de 1 até N em ordem decrescente.");
+		System.out.println();
+
+		System.out.print("Entre com um número: ");
+		int N = sc.nextInt();
+
+		for (int i = N; i >= 0; i -= 2) {
+				System.out.println(i);
+			}		
+		sc.close();
+	}
+}
