9b0a049d883173bf49b1cb9413872ec3a29f07e0
Paulinho <paulocardosoassessor@gmail.com>
2023-12-01 22:11:24 -0300

    Exercicios 118 - Faça um programa que some todos os números naturais abaixo de 1000 que são múltiplos de 3 ou 5

---
+++ Projetos/Exercicio_118_aula_19_Divisores_de_1000/src/aula_19/Programa.java
@@ -0,0 +1,32 @@
+package aula_19;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Faça um programa que some todos os números naturais abaixo de 1000 que são múltiplos de 3 ou 5.");
+		System.out.println();
+
+		int soma = 0;
+		int quantidadeDivisores = 0;
+
+                for (int i = 1; i <= 1000; i++) {
+                    if (i % 3 == 0 || i % 5 == 0) {
+                        soma += i;
+                        quantidadeDivisores ++;
+                        System.err.print(i + " ");
+                    }
+                    
+                }
+                System.out.println();
+                System.out.println();
+                System.out.println("Quantidade de divisores   : " + quantidadeDivisores);
+                System.out.println("Soma de todos os divisores: " + soma);
+}
+	}
