a00822953abd707cd5f711a2db9acc3fde05c415
Paulinho <paulocardosoassessor@gmail.com>
2024-01-03 15:38:36 -0300

    Aulas 88 - Escrevendo em arquivo txt

---
+++ Projetos/Aula_88_Leitura_e_Escrita_em_arquivos/src/string/Programa.java
@@ -0,0 +1,34 @@
+package string;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* Escrevendo em arquivos  */
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Scanner teclado = new Scanner(System.in);
+
+		try {
+			// Criar um arquivo e escrever dentro dele
+			PrintStream escrever = new PrintStream("saída.txt");
+
+			// SE PRECISAR DE ESCREVER MAIS DE UMA LINHA TEM QUE FAZER UM FOR
+			for (int i = 0; i < 5; i++) {
+
+				System.out.print("Escreva alguma mensagem #" + (i + 1) + ": ");
+				String msg = teclado.nextLine();
+				escrever.println(msg);
+			}
+			escrever.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Não foi possivel criar o arquivo...");
+		} finally {
+			System.out.println("Processo finalizado com sucesso...");
+		}
+		teclado.close();
+	}
+}
