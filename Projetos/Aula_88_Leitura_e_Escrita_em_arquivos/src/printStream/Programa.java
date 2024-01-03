8a4584f6a1fc823f2f32bc8c049b49598fb4ccdc
Paulinho <paulocardosoassessor@gmail.com>
2024-01-03 15:55:15 -0300

    Aulas 89 - Lendo arquivo txt

--- Projetos/Aula_88_Leitura_e_Escrita_em_arquivos/src/printStream/Programa.java
+++ Projetos/Aula_88_Leitura_e_Escrita_em_arquivos/src/printStream/Programa.java
@@ -1,4 +1,4 @@
-package string;
+package printStream;
 
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
---
+++ Projetos/Aula_89_Escrevendo_em_arquivos/src/fileInputStream/Programa.java
@@ -0,0 +1,43 @@
+package fileInputStream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/* Lendo em arquivos  */
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		try {
+			// Ler o arquivo
+			Scanner ler1 = new Scanner(new FileInputStream("saída.txt"));
+			Scanner ler2 = new Scanner(new FileInputStream("entrada.txt"));
+
+			// Ler linha por linha (hasNextLine -> enquento tiver próxima linha)
+			while (ler1.hasNextLine()) {
+				String linha = ler1.nextLine();
+				System.out.println(linha);
+			}
+			System.out.println();
+			
+			while (ler2.hasNextLine()) {
+				String linha = ler2.nextLine();
+				System.out.println(linha);
+			}
+			ler1.close();
+			ler2.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado...");
+		} 
+		
+		
+		finally {
+			System.out.println();
+			System.out.println("Processo finalizado com sucesso...");
+		}
+
+	}
+}
